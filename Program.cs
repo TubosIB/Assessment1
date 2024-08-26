9e89b1870cb779a4ff88dd36641b3c578fc8fff9
olatubosun ibrahim <olatubosunibrahim10@gmail.com>
2024-08-26 17:01:23 +0100

    MyFirstCommit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿// Question 2:Sum of two number
+
+// Console.WriteLine("Enter number: ");
+// int i=Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter number to be added: ");
+// int a=Convert.ToInt32(Console.ReadLine());
+// int p =i+a;
+// Console.WriteLine("sum of digit :"  + p)
+
+//Q 5
+// Console.WriteLine("First Number: ");
+// int y=Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Second Number: ");
+// int z=Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Third Number: ");
+// int s=Convert.ToInt32(Console.ReadLine());
+// if (y>z)
+// {
+//     Console.WriteLine("The Largest is: "+ Math.Max(y,s));
+// }
+// if (z>s)
+// {
+//     Console.WriteLine("The Largest is: " + Math.Max(z,y));
+// }
+// if (s>y)
+// {
+//     Console.WriteLine("The Largest is: "+ Math.Max(s,z));
+// }
+
+//Q 3
+
+Console.WriteLine("Enter Number: ");
+int h=Convert.ToInt32(Console.ReadLine());
+if (h%2==0)
+{
+    Console.WriteLine("It is an even number");
+}
+else
+{
+    Console.WriteLine("It ia an old number ");
+}
+    
+
+    
+
